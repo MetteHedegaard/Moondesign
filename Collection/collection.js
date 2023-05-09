ad70234bdd32077ef731d190175874d424f4e91b
Mette <mett86q0@edu.zealand.dk>
2023-05-09 12:56:18 +0200

    change pages + icons in footer

--- Collection/collection.js
+++ Collection/collection.js
@@ -1,4 +1,4 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+/* Burgermenu Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
@@ -6,4 +6,13 @@ function myFunction() {
     } else {
       x.style.display = "block";
     }
+  }
+
+  /* collection page change numbers Toggle between showing and hiding*/
+  function changepagenumbers() {
+ var element = document.getElementsByClassName("grid1");
+    element.classList.toggle("hide");
+
+    var element = document.getElementsByClassName("grid2");
+    element.classList.toggle("hide");
   }
