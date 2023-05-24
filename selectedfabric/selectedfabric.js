d5b9402e73fb519c75f6b537ab67cf0b96d941e5
Mette <mett86q0@edu.zealand.dk>
2023-05-24 15:50:43 +0200

    updates efter kode cafe

--- selectedfabric/selectedfabric.js
+++ selectedfabric/selectedfabric.js
@@ -7,3 +7,27 @@
       x.style.display = "block";
     }
   }
+
+
+  /*Vi har en toggle function som har fire variabler, som der skiftes imellem ved onclick funktionen*/
+    /*block vises billedet og none vises billedet ikke*/
+  function toggleImage() {
+    var img1 = document.getElementById('img1');
+    var img2 = document.getElementById('img2');
+    var fab1 = document.getElementById('fab1');
+    var fab2 = document.getElementById('fab2');
+
+    if (img1.style.display && fab1.style.display === 'none') {
+        img1.style.display = 'block';
+        img2.style.display = 'none';
+        fab1.style.display = 'block';
+        fab2.style.display = 'none';
+    } else {
+        img1.style.display = 'none';
+        img2.style.display = 'block';
+        fab1.style.display = 'none';
+        fab2.style.display = 'block';
+
+    }
+}
+
--- tailorchosenitem/tailorchosenitem.js
+++ tailorchosenitem/tailorchosenitem.js
@@ -1,9 +1,34 @@
- /* Burgermenu Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
- function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+/* Burgermenu Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
+
+
+// function colorFunction() {
+//   document.getElementById("imgOne").style.filter = "grayscale(0%)";
+//   document.getElementById("imgTwo").style.filter = "grayscale(0%)";
+//   document.getElementById("imgThree").style.filter = "grayscale(0%)";
+// }
+
+
+// function colorFunction(element) {
+//   // Remove the grayscale filter from the clicked image
+//   element.style.filter = "grayscale(0%)";
+
+//   // Apply the grayscale filter to the other images - variablen er 'images' og billedet hentes ved 'this' i gridtailor klassen
+//   // for = forloop 
+//   // variablen I er = 0 
+//   // if variablen 'images' ikke er det samme som 'element'  og element er = 0, hvis ikke det er det, så skal den have farve
+//   var images = document.getElementsByClassName("gridtailor")[0].getElementsByTagName("img");
+//   for (var i = 0; i < images.length; i++) {
+//     if (images[i] !== element) {
+//       images[i].style.filter = "grayscale(100%)";
+//     }
+//   }
+// }
+
